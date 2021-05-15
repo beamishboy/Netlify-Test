9076e7a1a0ba248a00d730cf7551f9a41d07c18a
Sir Bird <sirjubjubbird@gmail.com>
2021-05-15 18:02:17 +0530

    Batch publishing changes

--- myfunctions/serverUpdateString.js
+++ myfunctions/serverUpdateString.js
@@ -6,7 +6,7 @@ exports.handler = async function (event) {
     const initString = event.queryStringParameters.input;
 
 
-    // const rootUnicode = initString.charCodeAt(0);
+    const rootUnicode = initString.charCodeAt(0);
     // const finalUnicode = "A".charCodeAt(0) + (rootUnicode - "A".charCodeAt(0) + alphabetSeparation) % 26;
     // const finalChar = String.fromCharCode(finalUnicode);
 
