8e4cfd24f155f8ca6f5d590ea4233a8c87ba70d5
Sir Bird <sirjubjubbird@gmail.com>
2021-05-15 19:15:53 +0530

    Debugging

--- myfunctions/serverUpdateString.js
+++ myfunctions/serverUpdateString.js
@@ -12,7 +12,7 @@ exports.handler = async function (event) {
     const stringArray = initString.split("");
     //stringArray[charsBetween + 1] = finalChar;
 
-    const stringToReturn = stringArray[charsBetween + 3];
+    const stringToReturn = stringArray[charsBetween];
 
 
     const response = {
