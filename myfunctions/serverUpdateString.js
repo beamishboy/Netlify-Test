9211ae5b6742cab1e1f79ef1e60fb92614608d8c
Sir Bird <sirjubjubbird@gmail.com>
2021-05-16 14:20:47 +0530

    Debugging

--- myfunctions/serverUpdateString.js
+++ myfunctions/serverUpdateString.js
@@ -1,26 +1,26 @@
 exports.handler = async function (event) {
 
-    const { CHARS_BETWEEN, ALPHABET_SEPARATION } = process.env;
-    const charsBetween = parseInt(CHARS_BETWEEN);
-    const alphabetSeparation = parseInt(ALPHABET_SEPARATION);
-    const initString = event.queryStringParameters.input;
+    // const { CHARS_BETWEEN, ALPHABET_SEPARATION } = process.env;
+    // const charsBetween = parseInt(CHARS_BETWEEN);
+    // const alphabetSeparation = parseInt(ALPHABET_SEPARATION);
+    // const initString = event.queryStringParameters.input;
 
-    const rootUnicode = initString.charCodeAt(0);
-    const finalUnicode = "A".charCodeAt(0) + (rootUnicode - "A".charCodeAt(0) + alphabetSeparation) % 26;
+    // const rootUnicode = initString.charCodeAt(0);
+    // const finalUnicode = "A".charCodeAt(0) + (rootUnicode - "A".charCodeAt(0) + alphabetSeparation) % 26;
 
 
-    const finalChar = String.fromCharCode(finalUnicode);
+    // const finalChar = String.fromCharCode(finalUnicode);
 
-    const stringArray = initString.split("");
-    const index = charsBetween + 1;
-    stringArray[index] = finalChar;
+    // const stringArray = initString.split("");
+    // const index = charsBetween + 1;
+    // stringArray[index] = finalChar;
 
-    const stringToReturn = stringArray.join("") + "#SIG#"
+    // const stringToReturn = stringArray.join("") + "#SIG#"
 
 
     const response = {
         statusCode: 200,
-        body: JSON.stringify(stringToReturn),
+        body: JSON.stringify("#SIGNATURERESPONSE"),
     }
 
     return response;
