2fae499ece48f9be7c64ff273d939b90ca2b81c0
Sir Bird <sirjubjubbird@gmail.com>
2021-05-16 14:39:33 +0530

    Debugging

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ async function postRandomString() {
         const res = await fetch(`https://netlifytestserverless.netlify.app/.netlify/functions/serverUpdateString?input=${stringToUpdate}`, config);
         updateVisuals("1", "1", "1");
         console.log(res);
-        console.log(res.body)
+        console.log("res.body: " + res.body);
         const data = await res.json();
         updateVisuals("2", "2", "2");
         console.log(data);
